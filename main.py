c677dec5bee52928976dcab69ba5ade42facf74e
arkn <ken.arai.jp@gmail.com>
2020-02-28 02:35:29 +0900

    added new file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
+import os
 import importlib
 import json
 
-f = open('config/test.json', 'r')
-j = json.load(f)
-print(j)
+for filename in os.listdir('config/.'):
+  f = open('config/' + filename, 'r')
+  j = json.load(f)
+  
+  plugin_pkg_name = 'plugin.' + j['plugin']['pkg']
 
-plugin_pkg_name = 'plugin.' + j['plugin']['pkg']
+  m = importlib.import_module(plugin_pkg_name)
 
-m = importlib.import_module(plugin_pkg_name)
-
-m.sayhello()
+  m.sayhello()
